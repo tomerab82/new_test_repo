afa2a4f5992c64071abffd2bf64a979791dbd96b
Tomer Abrahamm <tomer.abr1@gmail.com>
2026-08-30 23:04:23 +1000

    add 20m run

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -15,7 +15,7 @@ profile_config = ProfileConfig(
 
     ))
 
-dbt_snowflake_dag = DbtDag(
+dbt_snowflake_dag_hr_run = DbtDag(
     project_config=ProjectConfig("/usr/local/airflow/dags/dbt/tomer_cdp",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
@@ -24,5 +24,17 @@ dbt_snowflake_dag = DbtDag(
     schedule="0 * * * *",
     start_date=datetime(2023, 1, 1),
     catchup=False,
-    dag_id="dbt_dag",
+    dag_id="dbt_dag_hr_run",
+)
+
+dbt_snowflake_dag_20m_run = DbtDag(
+    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/tomer_cdp",),
+    operator_args={"install_deps": True},
+    profile_config=profile_config,
+    render_config=RenderConfig(select=["tag:20m_run"]),
+    execution_config=ExecutionConfig(execution_mode=ExecutionMode.LOCAL,dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt"),
+    schedule="*/20 * * * *",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="dbt_dag_20m_run",
 )
