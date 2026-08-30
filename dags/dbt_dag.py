417ed2a0752197e00b67868a90dc45ba4382f072
Tomer Abrahamm <tomer.abr1@gmail.com>
2026-08-30 22:04:39 +1000

    update cron and create new fact

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -24,7 +24,7 @@ dbt_snowflake_dag = DbtDag(
         # 👇 CHANGED THIS PATH to point to your Dockerfile's environment
         dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt" 
     ),
-    schedule="@daily",
+    schedule="*/5 * * * *",
     start_date=datetime(2023, 1, 1),
     catchup=False,
     dag_id="dbt_dag",
