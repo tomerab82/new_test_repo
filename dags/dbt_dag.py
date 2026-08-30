42a18be6563125478da4cfc2c5de0c7ac209fc48
Tomer Abrahamm <tomer.abr1@gmail.com>
2026-08-30 22:57:12 +1000

    fix hour run code

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 from cosmos.constants import ExecutionMode
-from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig, SelectConfig, ExecutionConfig
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
 
@@ -19,7 +19,7 @@ dbt_snowflake_dag = DbtDag(
     project_config=ProjectConfig("/usr/local/airflow/dags/dbt/tomer_cdp",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
-    render_config=RenderConfig(select=SelectConfig(tags=["hr_run"])),
+    render_config=RenderConfig(select=["tag:[hr_run"]),
     execution_config=ExecutionConfig(execution_mode=ExecutionMode.LOCAL,dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt"),
     schedule="*/0 * * * *",
     start_date=datetime(2023, 1, 1),
